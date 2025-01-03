fb96838452e89d9ff0546850dae26d4c324babed
mark.lynch <mark.lynch@ladbrokes.com.au>
2025-01-03 21:41:12 +1000

    seperate files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ensure-kube-context
+
 A ZSH plugin to ensure the --context flag is passed to kubectl.
+
+## Install
+
+Oh My Zsh
+
+Clone the repository:
+
+```bash
+git clone --depth=1 https://github.com/do-i-need-a-username/ensure-kube-context.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/ensure-kube-context
+```
+
+Open ~/.zshrc and set ENSURE_KUBE_CONTEXT_COMMANDS to a list of commands you want to force the --context flag on.
+
+```bash
+# .zshrc
+ENSURE_KUBE_CONTEXT_COMMANDS=(kubectl cilium stern)
+```
---
+++ ensure-kube-context/ensure-kube-context.plugin.zsh
@@ -0,0 +1,17 @@
+# ensure-kube-context.plugin.zsh
+for module in "${ENSURE_KUBE_CONTEXT_MODULES[@]}"; do
+  case $module in
+    kubectl)
+      command -v kubectl >/dev/null 2>&1 && source "${0:A:h}/ensure-kube-context-kubectl.plugin.zsh"
+      ;;
+    cilium)
+      command -v cilium >/dev/null 2>&1 && source "${0:A:h}/ensure-kube-context-cillium.plugin.zsh"
+      ;;
+    stern)
+      command -v stern >/dev/null 2>&1 && source "${0:A:h}/ensure-kube-context-stern.plugin.zsh"
+      ;;
+    *)
+      echo "Unknown module in ENSURE_KUBE_CONTEXT_MODULES: $module"
+      ;;
+  esac
+done
